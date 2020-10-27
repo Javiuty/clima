4304509c522ff8119a00e4592835454d46967243
Javiuty <javiiescuadra@gmail.com>
2020-10-27 17:06:34 +0100

    adding documentation to the app

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
     const consultarAPI = async () => {
       if (consultar) {
         const appId = API_KEY;
-        const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
 
         const respuesta = await fetch(url);
         const resultado = await respuesta.json();
@@ -39,6 +39,7 @@ function App() {
       }
     };
     consultarAPI();
+    // eslint-disable-next-line
   }, [consultar]);
 
   let componente;
